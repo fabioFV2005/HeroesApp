7afccd7440cbeecdfd9976ea58737bfc38a0f415
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-01-30 18:54:41 -0400

    inicializacion heroes app

---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,13 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
+const SearchPage = () => {
+    return (
+        <>
+            <CustomJumbotron
+                title="Busqueda de superHeroes"
+                description="Descubre, explora y administra super heroes y villanos"
+            />
+        </>
+    )
+}
+export default SearchPage;
---
+++ src/router/app.router.tsx
@@ -0,0 +1,44 @@
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { HomePage } from "@/heroes/pages/home/HomePage"
+import { createBrowserRouter } from "react-router";
+// import { SearchPage } from "";
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { lazy } from "react";
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
+export const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <HeroesLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: "/heroes/1",
+                element: <HeroPage />
+            },
+            {
+                path: "/search",
+                element: <SearchPage />
+            },
+        ],
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <AdminPage />
+            }
+        ],
+    },
+    {
+        path: "*",
+        element: <h1>404 - Not Found</h1>
+    }
+
+]);
