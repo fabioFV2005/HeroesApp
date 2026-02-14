fa689610e31509fa8cf3c284b0c42eb6ba1a82fb
Fabio Fernandez <kingofnoobs845@gmail.com>
2026-02-14 00:04:34 -0400

    tanstackquery

--- src/HeroesApp.tsx
+++ src/HeroesApp.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./router/app.router"
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 export const HeroesApp = () => {
+    const queryClient = new QueryClient()
     return (
-        <RouterProvider router={appRouter} />
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <RouterProvider router={appRouter} />
+        </QueryClientProvider>
     )
 
 }
--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -2,13 +2,22 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import { CustomJumbotron } from '../../../components/custom/CustomJumbotron';
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CustomPagination } from '../../../components/custom/CustomPagination';
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomePage = () => {
     const [activeTab, setActiveTab] = useState<"all" | "favorites" | "heroes" | "villains">("all");
-
+    const { data } = useQuery({
+        queryKey: ['heroes'],
+        queryFn: () => getHeroesByPageAction(),
+        staleTime: 1000 * 60 * 5
+    })
+    // useEffect(() => {
+    //     getHeroesByPage().then()
+    // }, [])
 
     return (
         <>
